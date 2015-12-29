736330179961681db1afa4216c7cff3e727cae22
v.shago <dpatriot@gmail.com>
2015-12-29 12:54:54 +0300

    no message

--- init-cron.sh
+++ init-cron.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$S3_CRON_FILE" ]; then echo "S3_CRON_FILE is unset"; exit 1; fi
 
-aws s3 cp $S3_CRON_FILE /etc/cron.d/
+aws s3 cp $S3_CRON_FILE /etc/cron.d/cron-jobs
 chmod 0644 /etc/cron.d/cron-jobs
 
 touch /var/log/cron.log
